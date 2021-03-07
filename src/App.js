c5eb97a4e8efedc12546b7bc7e9bbd9aeb06ed5e
ARNAUD <arnaud@duhamel.biz>
2021-03-07 17:49:04 +0100

    Added Kraken API call (getTradesHistory, addOrder) Refactored API calls Added pairs consts
    Added React Component TradesHistory.js
    
    Added Vitamin library for tests
    
    Added function to pass order

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import Ticker from './components/Ticker';
 import Balance from './components/Balance';
+import TradesHistory from './components/TradesHistory';
 
 import './App.css';
 import 'typeface-roboto';
 import 'typeface-roboto-condensed';
 
-
 import Kraken from './components/Kraken';
 import { VtmnButton } from '@vtmn/react';
+import homeline from '@vtmn/icons/dist/vitamix/svg/home-line.svg';
+/*import playline from '@vtmn/icons/dist/vitamix/svg/play-line.svg';
+import pauseline from '@vtmn/icons/dist/vitamix/svg/pause-line.svg';*/
 
 const kraken = new Kraken();
 
@@ -16,6 +19,7 @@ const App = () => {
  
   const [ticker, setTicker] = useState(null);
   const [balance, setBalance] = useState(null);
+  const [tradesHistory, setTradesHistory] = useState(null);
   const [autorefresh, setAutoRefresh] = useState('active');
 
   useEffect( () => {
@@ -31,13 +35,13 @@ const App = () => {
 
   const updateTicker = async() => {  
     let newTicker={
-      pair: 'XXBTZEUR',
+      pair: 'BCHEUR',
       //lastData: null,
       //data: null
     };
 
     const result = await kraken.getTicker(newTicker.pair);
-    newTicker.data = result.data.result.XXBTZEUR;
+    newTicker.data = result.data.result.BCHEUR;
 
     // Save the last result, before to update data
     try {
@@ -58,6 +62,26 @@ const App = () => {
     setBalance(balance);
   }
 
+  const updateTradesHistory = async() => {  
+    const result = await kraken.getTradesHistory('');
+    let tradesHistory = result.data.result;
+
+    setTradesHistory(tradesHistory);
+  }
+
+  const sendAddOrder = async(asset, pair, type, orderType, price, volume) => {
+    let result = await kraken.getBalance('');
+    let balance = result.data.result;
+
+    console.log('sendAddOrder - balance[pair]',balance);
+    console.log('sendAddOrder - balance[pair]',balance[asset]);
+
+    result = await kraken.addOrder(pair, type, orderType, price, balance[asset]);
+    let addOrder = result.data.result;
+
+    console.log('sendAddOrder',addOrder);
+  }
+
   const handleTickerClick = async() => {  
     console.log('App', 'handleTickerClick');
     updateTicker();
@@ -68,18 +92,31 @@ const App = () => {
     updateBalance();
   }
 
-/*  const componentDidMount = async() => {
-    console.log('App', 'componentDidMount');
-    setTicker();
-    setBalance();
-  }*/
+  const handleTradesHistoryClick = async() => {  
+    console.log('App', 'handleTradesHistoryClick');
+    updateTradesHistory();
+  }
+
+  const handleOnSell = async(asset, pair, price, volume) => {
+    console.log('App', 'handleOnSell ' + pair + ' at ' + price + 'for ' + volume);
+    sendAddOrder(asset, pair, 'sell', 'limit', price.toFixed(2), volume);
+  }
 
   return (    
       <div className="App">
+        <div>
+          <img
+            key="home-line" src={homeline}
+            alt="home-line" width="32" height="32"
+          />
+        </div>
         <Ticker ticker={ticker} onClick={handleTickerClick}/>
         <Balance balance={balance} onClick={handleBalanceClick}/>
-        <VtmnButton autorefresh={autorefresh} onClick={() => setAutoRefresh(autorefresh==='active'?'disable':'active')}>{autorefresh==='active'?'Stop Auto Refresh':'Start Auto Refresh'}</VtmnButton>
-      </div>
+        <TradesHistory tradesHistory={tradesHistory} onClick={handleTradesHistoryClick} onSell={handleOnSell}/>
+        <VtmnButton autorefresh={autorefresh} onClick={() => setAutoRefresh(autorefresh==='active'?'disable':'active')}>
+          {autorefresh==='active'?'Stop Auto Refresh':'Start Auto Refresh'}
+        </VtmnButton>
+      </div>  
     );
 }
 
