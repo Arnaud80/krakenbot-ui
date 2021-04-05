910395fea5f86de6579bb0be7b7dcc0771a4736d
ARNAUD <arnaud@duhamel.biz>
2021-04-05 16:58:16 +0200

    Corrected all smellcode of Kraken.js

--- src/components/Kraken.js
+++ src/components/Kraken.js
@@ -26,7 +26,7 @@ class Kraken {
     // Get Ticker from public Kraken API
     // pair : <the name of searched pair>
     getTicker = async(pair) => {
-        return(await axios.get(config.url + config.public.tickerURI + '?pair=' + pair));
+        return(axios.get(config.url + config.public.tickerURI + '?pair=' + pair));
     }
     
     getBalance = async() => {
@@ -70,8 +70,6 @@ class Kraken {
         var params = {
             'nonce' : api_nonce,
         };
-
-        //params.nonce = api_nonce; 
 
         var api_signature = this.getMessageSignature(api_path, params, private_key, api_nonce);
 
@@ -160,15 +158,5 @@ const ALTpairs = {
     'XDGEUR' : 'XDGEUR',
     'BCHEUR' : 'BCHEUR',
 }
-
-/*
-const test_dataTicker={
-  "error":[],
-  "result":
-      {"XXBTZEUR":
-          {"a":["33543.80000","1","1.000"],"b":["33543.20000","1","1.000"],"c":["33549.70000","0.09198674"],"v":["5600.11958776","6314.01816805"],"p":["33029.21759","32838.72615"],"t":[63739,76707],"l":["31719.40000","31009.20000"],"h":["34007.80000","34007.80000"],"o":"31809.60000"}
-      }
-}
-*/
 
 export {Kraken as default, assets, pairs, EURpairs, ALTpairs};
